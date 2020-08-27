7eaecec1f08887cb255f4e3a064df0fde1869610
devenderbutani21 <devenderbutani21@gmail.com>
2020-08-27 15:38:39 +0530

    Basic Structure

---
+++ android/app/src/main/kotlin/challenge5/challenge5/MainActivity.kt
@@ -0,0 +1,6 @@
+package challenge5.challenge5
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Challenge-5',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size deviceSize = MediaQuery.of(context).size;
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          height: deviceSize.height,
+          width: deviceSize.width,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
